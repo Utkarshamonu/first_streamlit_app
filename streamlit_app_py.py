80a410f6a0653f9a0096f354c6d6c9ce1336667e
Utkarsha <110667572+Utkarshamonu@users.noreply.github.com>
2022-08-05 16:31:30 +0530

    Update streamlit_app_py.py
    typo chng

--- streamlit_app_py.py
+++ streamlit_app_py.py
@@ -2,6 +2,6 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Pmeg 3 and Bluberry oat meal')
-streamlit.text('Kale,Spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled free range-egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
