9ba47fb1b1dd17fbc1a888b6548f7ae6b5e17645
Utkarsha <110667572+Utkarshamonu@users.noreply.github.com>
2022-08-05 16:20:18 +0530

    Update streamlit_app_py.py
    added cntent

--- streamlit_app_py.py
+++ streamlit_app_py.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My Parents New Healty Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Pmeg 3 and Bluberry oat meal')
+streamlit.text('Kale,Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled free range-egg')
