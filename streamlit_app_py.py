d742c09de0b8a09207ce1e882f6e143ec6950857
Utkarsha <110667572+Utkarshamonu@users.noreply.github.com>
2022-08-05 16:36:31 +0530

    Update streamlit_app_py.py
    header chng

--- streamlit_app_py.py
+++ streamlit_app_py.py
@@ -1,7 +1,7 @@
 
 import streamlit
-streamlit.title('My Parents New Healty Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Moms New Healty Diner')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
